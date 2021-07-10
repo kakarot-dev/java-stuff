4a400720763b63eeefb952f5ce4789ffdf38b513
Libni Kunjukutty <kakarot.joel@gmail.com>
2021-07-10 12:28:52 +0530

    Create strings.java

---
+++ strings/strings.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int i, a;
+        String s, s1, s2;
+        System.out.println("Enter a Sentence");
+        s = sc.nextLine();
+        s1 = s.toUpperCase();
+        s2 = "" + s1;
+        a = 0;
+        System.out.println(s1);
+    }
+}
